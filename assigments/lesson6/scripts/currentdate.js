b7542f942e021a1b0fde530cc10ddbfcc8437011
Leecit230 <Leecit230@gmail.com>
2018-02-17 21:22:13 -0600

    lesson 6 upload

--- assigments/lesson6/scripts/currentdate.js
+++ assigments/lesson6/scripts/currentdate.js
@@ -1,15 +1,5 @@
-tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
-
-function GetClock(){
-var d=new Date();
-var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getYear();
-if(nyear<1000) nyear+=1900;
-
-document.getElementById('clockbox').innerHTML=""+tday[nday]+", "+ndate+" "+tmonth[nmonth]+", "+nyear+"";
-}
-
-window.onload=function(){
-GetClock();
-setInterval(GetClock,1000);
-}
+var now = new Date();
+var month = ['January','February','March','April','May','June','July','August','September','October', 'November','December'];
+var day = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var fdate = day[now.getDay()] + ' ' + now.getDate() + ' ' + month[now.getMonth()] + ' ' + now.getFullYear();
+document.getElementById('current-date').innerHTML = fdate;
